21ae7bb121000bcc2c64527b73193e5b3f9541b7
antonio.devtred@gmail.com <antonio.devtred@gmail.com>
2026-02-08 23:34:54 +0100

    add abis

---
+++ FrontEnd/FrontEnd/src/contracts/addresses.ts
@@ -0,0 +1,6 @@
+// src/contracts/addresses.ts
+
+export const FACTORY_ADDRESS = "0x34A1D3fff3958843C43aD80F30b94c510645C316";
+
+// En el futuro:
+// export const DEFAULT_MARKET = "0x...";
---
+++ FrontEnd/FrontEnd/src/contracts/index.ts
@@ -0,0 +1,6 @@
+// src/contracts/index.ts
+
+export * from "./addresses";
+export * from "./marketFactoryAbi";
+
+import { FACTORY_ADDRESS, marketFactoryAbi } from "@/contracts";
